90fa41f1a61c9833ca48f8c601f3ea7ddd560460
roberto-palanga <r.palanga94@gmail.com>
2024-01-25 17:49:57 +0100

    Mezzo esercizio fatto

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const nome = prompt("Come ti chiami");
+
+document.getElementById("Name").innerHTML = ` Il tuo nome è ${nome}`
+
+// fine inserimento nome //
+
+// insierimento cognome //
+
+const surname = prompt("Indica il tuo congnome")
+
+document.getElementById("Surname").innerHTML = ` Il tuo cognome è ${surname}`
+
+// fine inserimento cognome //
+
+// inserimento colore //
+
+const color = prompt("Indica il tuo colore preferito")
+
+document.getElementById("Color").innerHTML = `Il tuo colore preferito è ${color}`
+
+// fine inserimento colore //
+
+
+
+
+
+
+
